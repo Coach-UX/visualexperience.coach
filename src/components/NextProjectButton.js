ad0a33d02a73450ea7187af05de5a72c741bf743
Bobby Marcus <r11marcus11@gmail.com>
2020-04-14 13:56:50 -0400

    Improved animation of "Next Project" button and transition to next page

--- src/components/NextProjectButton.js
+++ src/components/NextProjectButton.js
@@ -5,7 +5,7 @@ import DelayLink from 'react-delay-link';
 import styled from "styled-components";
 import media from "../Mixins";
 
-const NextProject = styled.h1`
+const NextProject = styled.h2`
   width: 100%;
   height: 30px;
   text-align: center;
@@ -13,17 +13,36 @@ const NextProject = styled.h1`
   cursor: pointer;
   position: fixed;
   bottom: -145px;
+  font-size: 25px;
+  transition: padding .2s ease-in-out, bottom .3s ease-in-out;
+  &:hover {
+    padding: 70px 0;
+    transition: padding .2s ease-in-out;
+  }
   ${media.sm`
     bottom: -210px;
     padding: 40px 0;
+    font-size: 20px;
+  `}
+  ${media.portrait`
+    bottom: -210px;
+    padding: 40px 0;
+    font-size: 20px;
   `}
 `;
 
 // gsap animation that triggers when button is clicked
 function nextProjectTransition() {
   var nextProjectAnim = gsap.timeline();
-  nextProjectAnim.to("#nextProject", .5, {height: "89vh", padding: "7vh 0 3vh", ease:Power2.easeInOut})
-                 .to("#nextProject", .5, {height: "30px", padding: "50px 0", ease:Power2.easeInOut});
+  {/* if (window.innerWidth > window.innerHeight) {
+    nextProjectAnim.to("#nextProject", .5, {whiteSpace: "pre-wrap", lineHeight: "7.5vw", fontSize:"8vw", padding: "19vh 0 77.8vh", ease:Power2.easeInOut});
+  }
+  else {
+    nextProjectAnim.to("#nextProject", .5, {whiteSpace: "pre-wrap", lineHeight: "48px", fontSize:"50px", padding: "22vh 0 74.3vh", ease:Power2.easeInOut});
+  } */}
+  nextProjectAnim.to("#nextProject", 0, {transition: "all 0s", ease:Power2.easeInOut}, 0)
+                 .to("#nextProject", .2, {color: "rgba(0,0,0,0)", ease:Power2.easeInOut},0)
+                 .to("#nextProject", .3, {padding: "19vh 0 77.8vh", ease:Power2.easeInOut});
   nextProjectAnim.play();
 }
 
@@ -32,7 +51,7 @@ if (window.innerWidth > window.innerHeight) {
   var nextProjectTriggerHook = "0.5"; // desktop
 }
 else {
-  var nextProjectTriggerHook = "1"; // mobile
+  var nextProjectTriggerHook = "0.7"; // mobile
 }
 
 export default class NextProjectButton extends React.Component {
