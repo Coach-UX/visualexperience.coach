99df8e22c8ee1c944d1a01d3ee297a97f12bbd40
Bobby Marcus <r11marcus11@gmail.com>
2020-04-14 13:55:22 -0400

    Made menu <DelayLink> a component for easier maintenance

---
+++ src/components/Menu/MenuLink.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import DelayLink from 'react-delay-link';
+import {gsap, Linear, Power2} from "gsap";
+import $ from 'jquery';
+
+function menuTransition() {
+  var theColorIs = $('.menuBackground').css("backgroundColor");
+  var menuBtnAnim = gsap.timeline();
+    menuBtnAnim.to("#menu", .3, {width: "100%", ease: Power2.easeIn})
+               .to("#menu", 0, {backgroundColor: "" + theColorIs + ""}, 0)
+               .to("li", 0, {pointerEvents: "none", opacity: ".3"}, 0)
+               .to(".menuItems", .3, {autoAlpha: 0, ease: Power2.easeInOut}, 0);
+    menuBtnAnim.play();
+    $('input[type="checkbox"]').prop('checked', false);
+}
+function menuTransition_gradient() {
+  var theColorIs = $('.menuBackground').css("backgroundImage");
+  var menuBtnAnim = gsap.timeline();
+    menuBtnAnim.to("#menu", .3, {width: "100%", ease: Power2.easeIn})
+               .to("#menu", 0, {backgroundImage: "" + theColorIs + ""}, 0)
+               .to("li", 0, {pointerEvents: "none", opacity: ".3"}, 0)
+               .to(".menuItems", .3, {autoAlpha: 0, ease: Power2.easeInOut}, 0);
+    menuBtnAnim.play();
+    $('input[type="checkbox"]').prop('checked', false);
+}
+
+class MenuLink_ extends Component {
+    render() {
+        if(this.props.history.location.pathname === this.props.to){
+            return <span>{this.props.linktext}</span>
+        }
+        return <DelayLink delay={300} clickAction={menuTransition} to={this.props.to}>{this.props.linktext}</DelayLink>
+    }
+}
+
+class MenuLink_Gradient extends Component {
+  render() {
+      if(this.props.history.location.pathname === this.props.to){
+          return <span>{this.props.linktext}</span>
+      }
+      return <DelayLink delay={300} clickAction={menuTransition_gradient} to={this.props.to}>{this.props.linktext}</DelayLink>
+  }
+}
+
+export const MenuLink = withRouter(MenuLink_);
+export const MenuLinkGradient = withRouter(MenuLink_Gradient);
