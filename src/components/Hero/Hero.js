af72853ba510a1fc5582bdaf37760a71300111e7
Bobby Marcus <r11marcus11@gmail.com>
2020-04-14 15:08:37 -0400

    Made hero section relatively positioned so that it's consistent across window sizes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,24 +2,41 @@ import React from 'react';
 import styled from "styled-components";
 import media from "../../Mixins";
 
-const IntroCopy = styled.h1`
-  grid-column: var(--gridSection-MD);
-  padding: 25% 0 15% 0;
+const HeroContainer = styled.div`
+position: relative;
+width: 100%;
+grid-column: var(--gridSection-MD);
+top: 0%;
+padding: 20% 0;
   ${media.sm`
-    padding: 10vh 0 13vh;
+    padding: 2vh 0 15vh;
+    padding: 50px 0 100px;
+  `}
+`;
+const IntroCopy = styled.h1`
+  position: relative;
+  width: 100%;
+  padding: 0;
+  text-align: left;
+  ${media.sm`
+    width: 80%;
+    padding: 0 10%;
   `}
 `;
 export class IntroSection extends React.Component {
   render() {
-    const { src } = this.props;
-    const { caption } = this.props;
     return (
-      <IntroCopy id="introCopy">
-        {this.props.children}
-      </IntroCopy>
+      <React.Fragment>
+        <HeroContainer>
+          <IntroCopy id="introCopy">
+            {this.props.children}
+          </IntroCopy>
+        </HeroContainer>
+      </React.Fragment>
     );
   }
 };
+
 
 const Hero = styled.img`
   grid-column: var(--gridSection-LG);
@@ -37,6 +54,37 @@ export class HeroImage extends React.Component {
       <React.Fragment>
         <Hero className="heroDesktop" src={"img/" + this.props.projectClass + "/hero.png"} />
         <Hero className="heroMobile" src={"img/" + this.props.projectClass + "/hero-mobile.png"} />
+      </React.Fragment>
+    );
+  }
+};
+
+const Video = styled.video`
+  grid-column: var(--gridSection-LG);
+  width: 60%;
+  z-index: 1;
+  position: relative;
+  object-fit: cover;
+  height: 80%;
+  margin: 0 20%;
+  box-shadow: 0 100px 70px -50px rgba(0,0,0,.15);
+  ${media.sm`
+    width: 80%;
+    margin: 0 10%;
+    height: 60%;
+  `}
+  ${media.portrait`
+    height: 60%;
+    width: 80%;
+    margin: 0 10%;
+  `}
+`;
+export class HeroVideo extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Video className="heroDesktop" src={"img/" + this.props.projectClass + "/hero.mp4"} autoPlay muted playsInline loop />
+        <Video className="heroMobile" src={"img/" + this.props.projectClass + "/hero.mp4"} autoPlay muted playsInline loop/>
       </React.Fragment>
     );
   }
