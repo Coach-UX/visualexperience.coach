43c80d5cdce7565c0fd260c93aa14e2968d95aae
Bobby Marcus <r11marcus11@gmail.com>
2020-05-02 13:31:42 -0400

    adjusted hover interaction

--- src/components/NextProjectButton.js
+++ src/components/NextProjectButton.js
@@ -7,37 +7,59 @@ import media from "../mixins/mixins.js";
 
 const NextProject = styled.h1`
   width: 100%;
-  height: 30px;
   text-align: center;
-  padding: 50px 0;
+  line-height: 1rem;
+  padding: 30px 0 0 0;
   cursor: pointer;
   position: fixed;
   bottom: -145px;
-  transition: padding .2s ease-in-out, bottom .3s ease-in-out;
+  color: rgba(0,0,0,0);
+  /* box-shadow: 0px 0px 30px rgba(0,0,0,0.1); */
+  transition: all .1s ease-in-out, padding .2s ease-in-out, bottom .3s ease-in-out;
+  & #nextProjectLabel {
+    display: block;
+    font-size: 1.2rem;
+    color: rgba(0,0,0,1);
+    opacity: 1;
+    transition: opacity .2s ease-in-out, line-height .2s ease-in-out;
+  }
    &:hover {
-     padding: 70px 0;
-     transition: padding .2s ease-in-out;
+     padding: 100px 0 75px 0;
+     line-height: 2rem;
+     transition: all .2s ease-in-out, color .5s ease-out;
+     color: rgba(0,0,0,1);
+     & > #nextProjectLabel {
+       opacity:0;
+       line-height: 0rem;
+       transition:  opacity .1s ease-in-out, line-height .2s ease-in-out;
+     }
    }
    ${media.portrait`
-    bottom: -210px;
-    padding: 40px 0;
-  `}
-  ${media.sm`
-    bottom: -210px;
-    padding: 40px 0;
+     bottom: -20vh;
+     padding: 5vh 0;
+     & #nextProjectLabel {
+       font-size: 1rem;
+     }
   `}
   ${media.touch`
+    line-height: 2rem;
+    color: rgba(0,0,0,1);
     &:hover {
-      padding: 40px 0;
+      padding: 5vh 0;
+      transition: all .2s ease-in-out, color .5s ease-out, bottom 0s;
+      & > #nextProjectLabel {
+        opacity:1;
+        line-height: 26px;
+      }
     }
  `}
 `;
-
 // gsap animation that triggers when button is clicked
 function nextProjectTransition() {
   var nextProjectAnim = gsap.timeline({paused:true});
   nextProjectAnim.to("#nextProject", 0, {transition: "all 0s", ease:Power2.easeInOut}, 0)
                   .to("#nextProject", .2, {color: "rgba(0,0,0,0)", ease:Power2.easeInOut},0)
+                  .to("#nextProjectLabel", .2, {color: "rgba(0,0,0,0)", ease:Power2.easeInOut},0)
                   .to("#progressBar", .2, {opacity: 0, ease:Power3.easeInOut},0)
                   .to("#nextProject", .3, {padding: "19vh 0 77.8vh", ease:Power2.easeInOut});
   nextProjectAnim.play();
@@ -53,13 +75,10 @@ else {
 
 export default class NextProjectButton extends React.Component {
   componentDidMount() {
-    // make button only appear once user reaches the end of the page
+    // make button appear once user reaches the end of the page
     const trigger = document.querySelector('.footer')
     const nextProjectTimeline = gsap.timeline({paused:true});
-    nextProjectTimeline.to("#nextProject", 0.3, {
-      bottom: 0,
-      ease:Power3.easeInOut
-    })
+    nextProjectTimeline.to("#nextProject", 0.3, {bottom: 0, ease:Power3.easeInOut},0)
     const nextProject = new ScrollScene({
       triggerElement: trigger, triggerHook: nextProjectTriggerHook,
       gsap: {
